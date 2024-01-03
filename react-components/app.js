f7e3afb6aa8fda0999061914cde5e3d0a7d03004
Gevorg <gevorgdavtyan99@mail.ru>
2024-01-03 18:35:33 +0400

    dd

--- react-components/app.js
+++ react-components/app.js
@@ -1,6 +1,16 @@
-import { Button } from "./button.js";
-
 const rootNode = document.getElementById("root");
+
+const Button = () => {
+  const [count, setCount] = React.useState(0);
+  const handleClick = () => setCount(count + 1);
+  return React.createElement(
+    "button",
+    {
+      onClick: handleClick,
+    },
+    `You clicked ${count} times!`
+  );
+};
 
 const App = () => {
   return React.createElement(
