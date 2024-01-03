78c9f202b7e9c76bf577d4815e0bfca4df9d94c2
Gevorg <gevorgdavtyan99@mail.ru>
2024-01-03 19:03:14 +0400

    d

--- react-components/app.js
+++ react-components/app.js
@@ -1,18 +1,6 @@
-import { sum } from "./button/index.js";
+import { Button } from "./button/index.js";
 
 const rootNode = document.getElementById("root");
-
-const Button = () => {
-  const [count, setCount] = React.useState(0);
-  const handleClick = () => setCount(count + 1);
-  return React.createElement(
-    "button",
-    {
-      onClick: handleClick,
-    },
-    `You clicked ${count} times!`
-  );
-};
 
 const App = () => {
   return React.createElement(
--- react-components/button/index.js
+++ react-components/button/index.js
@@ -1,3 +1,11 @@
-export function sum(a, b) {
-  return a + b;
-}
+export const Button = () => {
+  const [count, setCount] = React.useState(0);
+  const handleClick = () => setCount(count + 1);
+  return React.createElement(
+    "button",
+    {
+      onClick: handleClick,
+    },
+    `You clicked ${count} times!`
+  );
+};
