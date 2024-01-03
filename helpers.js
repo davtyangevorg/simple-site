7ef9d0b80964ab69750c37971c36606f26a53bb9
Gevorg <gevorgdavtyan99@mail.ru>
2024-01-03 17:51:26 +0400

    add logs

--- helpers.js
+++ helpers.js
@@ -1,3 +1,11 @@
 export function add(a, b) {
+  console.log("inside add function");
   return a + b;
+}
+
+console.log("inside helpers.js");
+
+export function subtract(a, b) {
+  console.log("inside subtract function");
+  return a - b;
 }
