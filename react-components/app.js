68d63811b66be616238d5fec51d53e12acff9bec
Gevorg <gevorgdavtyan99@mail.ru>
2024-01-03 20:12:00 +0400

    d

--- react-components/app.js
+++ react-components/app.js
@@ -13,6 +13,16 @@ const App = () => {
     getUserDataAsync();
   }, []);
 
+  React.useEffect(() => {
+    // Example usage of Atomics.waitAsync
+    const sharedBuffer = new SharedArrayBuffer(1024);
+    const sharedArray = new Int32Array(sharedBuffer);
+
+    Atomics.store(sharedArray, 0, 10);
+
+    const result = Atomics.waitAsync(sharedArray, 0, 10);
+    console.log("result", result);
+  }, []);
   return React.createElement(
     "div",
     null,
