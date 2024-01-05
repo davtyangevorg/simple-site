020a0faf002aa03f3d420688dbf809fe52108c2d
Gevorg <gevorgdavtyan99@mail.ru>
2024-01-05 15:07:44 +0400

    d

--- react-components/button/index.js
+++ react-components/button/index.js
@@ -3,13 +3,11 @@ import { useClickButton } from "./hooks.js";
 export const Button = () => {
   const { count, handleClick } = useClickButton();
 
-  return <div>vardan</div>;
-  // <button onClick={handleClick}>You clicked {count} times!</button>;
-  // return React.createElement(
-  //   "button",
-  //   {
-  //     onClick: handleClick,
-  //   },
-  //   `You clicked ${count} times!`
-  // );
+  return React.createElement(
+    "button",
+    {
+      onClick: handleClick,
+    },
+    `You clicked ${count} times!`
+  );
 };
